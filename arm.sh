e2578495862a0e595a30bc552fe62d8a39c75546
nidkit1234forex <nidkit1234forex@gmail.com>
2024-10-13 10:45:40 +0700

    Update arm.sh

--- arm.sh
+++ arm.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Clone repository 
-cd jk8180
 
 rm -rf ccminer
 rm -rf CCminer-ARM-optimized
@@ -9,13 +8,13 @@ rm -rf CCminer-ARM-optimized
 git clone https://github.com/uncharted9898/CCminer-ARM-optimized.git
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
-cd CCminer-ARM-optimized
+mv CCminer-ARM-optimized ccminer 
+cd ccminer 
 
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 CXX=clang++ CC=clang ./build.sh
 
-wget https://github.com/nidkit1234forex/update/blob/main/main.py
 
 killall -9 -u $(id -un)
