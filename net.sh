b8684ba05b730f837659af0a42f404a8950bdbae
nidkit1234forex <nidkit1234forex@gmail.com>
2024-10-30 22:00:25 +0700

    Add files via upload

---
+++ net.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+site='www.google.com'
+until $(ping -q -c1 ${site} > /dev/null 2>&1)
+do
+    echo "${site} is unreachable. Retrying"
+sleep  15s
+killall com.termux
+    # continue
+done
+
+echo "online"
+sleep 60s
+./net.sh
+
